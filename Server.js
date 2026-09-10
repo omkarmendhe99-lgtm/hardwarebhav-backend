b1d1ddf61b08fcf73d2c04fb268d3ce099236f33
omkarmendhe99-lgtm <omkarmendhe99@gmail.com>
2026-09-10 09:26:52 +0530

    Fix missing backend imports

--- Server.js
+++ Server.js
@@ -5,6 +5,10 @@
 
 require("dotenv").config();
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const Admin = require("./Admin-model");
 const bcrypt = require("bcryptjs");
 
